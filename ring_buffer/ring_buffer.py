7e484de9178fae05fe8a0e0fc4455c9df52e92e4
Elliot Gunn <elliot.j.gunn@gmail.com>
2020-02-02 19:11:45 -0500

    Add files via upload

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -11,15 +11,18 @@ class RingBuffer:
 
         # if max storage already, overwrite
         if self.storage.length == self.capacity:
-            # remove from head
-            self.storage.remove_from_head()
-            # add new item to tail
-            self.storage.add_to_tail(item)
+            self.current.value = item
+
+            if self.current == self.storage.tail:
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
 
         else:
             # just add to tail
             self.storage.add_to_tail(item)
-            #self.current = self.storage.head
+            # make the pointer add to the latest addition
+            self.current = self.storage.tail
             
     def get(self):
         # like the get_max() function
@@ -27,15 +30,13 @@ class RingBuffer:
         list_buffer_contents = []
 
         #look at the head first as it is the oldest
-        current = self.current
-        list_buffer_contents.append(current)
+        current = self.storage.head
 
-        while current != self.storage.head:
+        while current != None:
             # go to next node
-            current = current.next
-            # add to list
             list_buffer_contents.append(current)
-
+            current = current.next
+            
         return list_buffer_contents
 
 
